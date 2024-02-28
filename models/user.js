2bb06b389bf2e85ae9cdac03c8d7500ba361e5bd
JesusM97 <jissousklp@gmail.com>
2024-02-28 12:31:43 -0700

    fixed routing for profile view

--- models/user.js
+++ models/user.js
@@ -2,17 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    username: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true, min: 8}
-});
-
 const collectionSchema = new Schema({
     recipe: [{
         type: Schema.Types.ObjectId,
         ref: "Recipe"
     }]
 });
+const userSchema = new Schema({
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true, min: 8},
+    collection: {type: collectionSchema, required: true}
+});
+
 
 module.exports = mongoose.model("User", userSchema);
