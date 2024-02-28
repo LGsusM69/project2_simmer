b028eef8608beee90e1e59a9b1d156024c52b1ba
JesusM97 <jissousklp@gmail.com>
2024-02-28 09:42:43 -0700

    added save recipe to database functionality

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,9 +1,24 @@
 const Recipe = require("../models/recipe");
 
 module.exports = {
-new: newRecipe
+new: newRecipe,
+create
 };
 
 async function newRecipe(req, res) {
     res.render("recipes/new",);
+}
+
+async function create(req, res) {
+    // Remove empty properties so that defaults will be applied
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key];
+    }
+  try{
+    const recipe = await Recipe.create(req.body);
+    //res.redirect();
+  } catch(err) {
+    console.log(err);
+    //res.render("", {errorMsg: err.message});
+  }
 }
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const recipesCtrl = require("../controllers/recipes");
+//const { routes } = require("../server");
 
 
 router.get("/new", recipesCtrl.new);
+
+router.post("/", recipesCtrl.create);
 
 
 
