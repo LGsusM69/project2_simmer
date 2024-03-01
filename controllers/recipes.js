d10fcfb51c63f9a1157a0eec663b9ce4fa497961
JesusM97 <jissousklp@gmail.com>
2024-02-29 23:50:11 -0700

    details view works for api

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -15,13 +15,24 @@ async function newRecipe(req, res) {
 }
 
 async function create(req, res) {
+  console.log("create");
+  let template = req.body;
+  console.log(req.body);
+
+  //let owner;
+  if(req.user) {
+    template.owner = req.user._id;
+ 
+  }
     // Remove empty properties so that defaults will be applied
-    for (let key in req.body) {
-        if (req.body[key] === '') delete req.body[key];
+    for (let key in template) {
+        if (template[key] === '') delete req.body[key];
     }
+
   try{
-    const recipe = await Recipe.create(req.body);
-    //res.redirect();
+
+    const recipe = await Recipe.create(template);
+    res.redirect("/recipes/new");
   } catch(err) {
     console.log(err);
     //res.render("", {errorMsg: err.message});
@@ -29,59 +40,57 @@ async function create(req, res) {
 }
 
 async function show(req, res) {
-  //const recipe = await Recipe.findById(req.params.id).populate();
-  //console.log("elaidi: " + req.params.id);
 
-  //res.render("recipes/show", {title : recipe.title, recipe});
+  let dbRecipe;
+  let aPIRecipe;
+  try {
+    dbRecipe = await Recipe.findById(req.params.id).populate();
+  } catch(err) {
 
-  const { id } = req.params;
-   //console.log(`GET request received for /recipe/${id}`);
-   try {
-    
+  }
+
+  try{
+  } catch(err) {
+
+  }
+
+  try {
+    const { id } = req.params;
     const response = await Axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`)
-    const recipe = response.data;
-    res.render('recipes/show', { recipe });
+    aPIRecipe = response.data;
   } catch (error) {
-    console.log("arenita");
-       console.error('Error fetching recipe:', error.message);
-       res.status(500).send('failed get');
-   }
+
+  }
+  if(dbRecipe) res.render("recipes/showDB", {recipe: dbRecipe });
+  else if(aPIRecipe) res.render('recipes/showAPI', { recipe: aPIRecipe });
 }
 
 async function addToCollection(req, res) {
   console.log("add to collection");
-//get collection
-const user = req.user;
-//get recipeID"
-const recipeId = req.body.recipeId
-//get recipe:
-const recipe = await Recipe.findById(recipeId);
-//add recipe to userCollection
-//if(User.exists({collection: recipeId})) {
-  //console.log("ya existe we");
-  console.log("antes:");
+  //get collection
+  const user = req.user;
+  //get recipeID"
+  const recipeId = req.body.recipeId
+  //get recipe:
+  const recipe = await Recipe.findById(recipeId);
+  //add recipe to userCollection
   
-let exists = false;
-user.collection.forEach(function(element)
-  {
+  let exists = false;
+  user.collection.forEach(function(element)
+    {
     if(element == recipeId) exists = true;
   }
-);
-if(exists) return;
-  //console.log(exists);
+  );
+  if(exists) return;
 
-  console.log("despues");
-  //return;
-//}
-try {
+  try {
     user.collection.push(recipe);
-    // Save any changes made to the movie doc
+      // Save any changes made to the movie doc
     await user.save();
     res.redirect("/");
   } catch (err) {
     console.log("couldnt add to collection");
     console.log(err);
   }
-
-
 }
+
